d21e538edb96186b7fb2298179d3788bdd881b8a
Colin Key <colin.m.key@gmail.com>
2018-04-29 00:53:02 -0600

    Adding collapsing/expanding notepad functionality

--- src/components/notepad/Note.js
+++ src/components/notepad/Note.js
@@ -1,21 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./Note.css";
 
-const Note = props => (
-  <div className={props.expanded ? "note-container card" : "note-container card collapsed"}>
-    <h2 className="note-title">{props.noteTitle}</h2>
-    <p className="note-body">{props.noteBody}</p>
-    <div className="note-controls">
-      {props.expanded ? (
-        <i className="fas fa-angle-double-up" />
-      ) : (
-        <i className="fas fa-angle-double-down" />
-      )}
-      <i className="fas fa-edit" />
-      <i className="fas fa-ban" />
-    </div>
-  </div>
-);
+class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpandable: props.noteBody.length > 240,
+      expanded: false
+    };
+  }
+
+  toggleExpandedNote = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  };
+
+  render() {
+    return (
+      <div
+        className={this.state.expanded ? "note-container card" : "note-container card collapsed"}
+      >
+        <h2 className="note-title">{this.props.noteTitle}</h2>
+        <p className="note-body">
+          {this.props.noteBody.length > 240 && !this.state.expanded
+            ? `${this.props.noteBody.substring(0, 240)}...`
+            : this.props.noteBody}
+        </p>
+        <div className="note-controls">
+          {this.state.isExpandable ? (
+            <button onClick={this.toggleExpandedNote}>
+              <i className={`fas fa-angle-double-${this.state.expanded ? "up" : "down"}`} />
+            </button>
+          ) : null}
+          <button>
+            <i className="fas fa-edit" />
+          </button>
+          <button>
+            <i className="fas fa-ban" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+// const Note = props => (
+//   <div className={props.expanded ? "note-container card" : "note-container card collapsed"}>
+//     <h2 className="note-title">{props.noteTitle}</h2>
+//     <p className="note-body">
+//       {props.noteBody.length > 240 && !props.expanded
+//         ? `${props.noteBody.substring(0, 240)}...`
+//         : props.noteBody}
+//     </p>
+//     <div className="note-controls">
+//       {props.noteBody.length > 240 ? (
+//         props.expanded ? (
+//           <i className="fas fa-angle-double-up" onClick={() => props.toggleExpandedNote} />
+//         ) : (
+//           <i className="fas fa-angle-double-down" onClick={() => props.toggleExpandedNote} />
+//         )
+//       ) : null}
+//       <i className="fas fa-edit" />
+//       <i className="fas fa-ban" />
+//     </div>
+//   </div>
+// );
 
 export default Note;
--- src/components/notepad/Notepad.js
+++ src/components/notepad/Notepad.js
@@ -6,22 +6,21 @@ import "./Notepad.css";
 
 const Notepad = props => (
   <div className="module module-notepad">
-    <Note noteTitle={"Hello"} noteBody={"This is the note body"} expanded={false} />
-    <Note
-      noteTitle={"Hello"}
-      noteBody={
-        "This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body "
-      }
-      expanded={true}
-    />
+    <Note noteTitle={"Hello"} noteBody={"This is the note body"} />
     <Note
       noteTitle={"Hello"}
       noteBody={
         "This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body "
       }
     />
-    <Note noteTitle={"Hello"} noteBody={"This is the note body"} expanded={true} />
-    <Note noteTitle={"Hello"} noteBody={"This is the note body"} expanded={false} />
+    <Note
+      noteTitle={"Hello"}
+      noteBody={
+        "This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body This is the note body "
+      }
+    />
+    <Note noteTitle={"Hello"} noteBody={"This is the note body"} />
+    <Note noteTitle={"Hello"} noteBody={"This is the note body"} />
   </div>
 );
 
